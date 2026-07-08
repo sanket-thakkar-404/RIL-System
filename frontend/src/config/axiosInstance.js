bb30e01d7a9dc0f13a2668a9b9fb3af83faa1dba
sanket-thakkar-404 <sanketthakkar500@gmail.com>
2026-07-08 15:49:10 +0530

    frontend change

--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3002/api",
+  baseURL: "https://ril-system.onrender.com/api",
   withCredentials: true,
 })
 
